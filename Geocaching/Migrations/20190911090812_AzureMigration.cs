33155d4d9d9ff5814ce56ba3c62ff7c344a63b21
tomasvaro <tomas.varo63@gmail.com>
2019-12-16 20:54:33 +0100

    no message

---
+++ Geocaching/Migrations/20190911090812_AzureMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Geocaching.Migrations
+{
+    public partial class AzureMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
